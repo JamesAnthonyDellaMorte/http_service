8ca3bf7231d32b3bdf1f8fbdb5d46af32cd8ac05
James DellaMorte <james.a@popotomodem.com>
2024-07-15 16:19:06 -0400

    Works better

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,74 @@
 use futures::{FutureExt, SinkExt, StreamExt};
-use warp::http::StatusCode;
+use serde::{Deserialize, Serialize};
 use tokio::{
+    fs::OpenOptions,
+    io::AsyncWriteExt,
     process::Command,
     sync::broadcast,
     time::{interval, Duration},
 };
+use warp::http::StatusCode;
 use warp::{
     http::Uri,
     ws::{Message, WebSocket, Ws},
     Filter,
 };
 
+#[derive(Deserialize, Serialize)]
+struct UpdateContent {
+    content: String,
+}
+
+async fn update_and_restart_handler(
+    body: UpdateContent,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    match update_tvlimit_and_restart_service(&body.content).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(e) => {
+            eprintln!("Failed to update and restart: {:?}", e);
+            Ok(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+async fn update_tvlimit_and_restart_service(
+    contents: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Stop the tvlimit.service
+    Command::new("systemctl")
+        .arg("stop")
+        .arg("tvlimit.service")
+        .output()
+        .await?;
+
+    let file_path = "/root/tvlimit/tvtimer.txt";
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(file_path)
+        .await?;
+
+    file.write_all(contents.as_bytes()).await?;
+    file.sync_all().await?;
+
+    // Start the tvlimit.service
+    Command::new("systemctl")
+        .arg("start")
+        .arg("tvlimit.service")
+        .output()
+        .await?;
+
+    Ok(())
+}
 
 async fn service_status_sender(tx: broadcast::Sender<String>) {
-    let mut interval = interval(Duration::from_secs(1)); // Check every 5 seconds
+    let mut interval = interval(Duration::from_secs(1)); // Check every second
 
     loop {
         interval.tick().await;
         let output = Command::new("systemctl")
             .arg("status")
-            .arg("tvlimit.service") // Replace with your systemd service name
+            .arg("tvlimit.service")
             .output()
             .await
             .expect("Failed to execute systemctl");
@@ -28,10 +77,11 @@ async fn service_status_sender(tx: broadcast::Sender<String>) {
         tx.send(status).unwrap_or_default();
     }
 }
+
 async fn restart_service() -> Result<impl warp::Reply, warp::Rejection> {
     let output = Command::new("systemctl")
         .arg("restart")
-        .arg("tvlimit.service") // Replace with your systemd service name
+        .arg("tvlimit.service")
         .output()
         .await;
 
@@ -40,6 +90,33 @@ async fn restart_service() -> Result<impl warp::Reply, warp::Rejection> {
         Err(_) => Ok(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
+
+async fn stop_service() -> Result<impl warp::Reply, warp::Rejection> {
+    let output = Command::new("systemctl")
+        .arg("stop")
+        .arg("tvlimit.service")
+        .output()
+        .await;
+
+    match output {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Ok(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+async fn start_service() -> Result<impl warp::Reply, warp::Rejection> {
+    let output = Command::new("systemctl")
+        .arg("start")
+        .arg("tvlimit.service")
+        .output()
+        .await;
+
+    match output {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Ok(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
 async fn user_connected(ws: WebSocket, mut rx: broadcast::Receiver<String>) {
     let (mut ws_tx, _) = ws.split();
     while let Ok(status) = rx.recv().await {
@@ -61,17 +138,32 @@ async fn main() {
         })
     });
 
-    // Route to serve static/index.html for the root path
+    let update_and_restart_route = warp::path("update-and-restart")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and_then(update_and_restart_handler);
 
-    // Inside your main function, before starting the server, add a route for static files:
     let static_route = warp::path("static").and(warp::fs::dir("./static"));
     let redirect_route =
         warp::path::end().map(|| warp::redirect(Uri::from_static("/static/index.html")));
-    // Include this route in your warp::serve call alongside the existing WebSocket route
+
     let restart_route = warp::path("restart")
-    .and(warp::post())
-    .and_then(restart_service);
-    let routes = status_route.or(static_route).or(redirect_route).or(restart_route);
-    
+        .and(warp::post())
+        .and_then(restart_service);
+
+    let stop_route = warp::path("stop").and(warp::post()).and_then(stop_service);
+
+    let start_route = warp::path("start")
+        .and(warp::post())
+        .and_then(start_service);
+
+    let routes = status_route
+        .or(static_route)
+        .or(update_and_restart_route)
+        .or(redirect_route)
+        .or(restart_route)
+        .or(stop_route)
+        .or(start_route);
+
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
